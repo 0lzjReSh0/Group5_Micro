66226dc369ec2f4d796fe97e4fc95aa15f882a23
naNa7mi <lzj452392120@163.com>
2024-03-09 14:58:26 +0200

    part

--- client.py
+++ client.py
@@ -4,14 +4,14 @@ def add_part():
     name = input("Enter part name: ")
     description = input("descripton: ")
     quantity = input("Enter the number: ")
-    part_data = {'name': name, 'description': description, 'quantity': quantity}
-    response = requests.post('http://localhost:3000/parts', json=part_data)
+    part = {'name': name, 'description': description, 'quantity': quantity}
+    response = requests.post('http://localhost:3000/parts', json=part)
     if response.status_code == 200:
         print("Success:", response.json())
     else:
         print("failed")
 
-def get_part():
+def get_part(): 
     name = input("Enter the name you want to search: ")
     response = requests.get(f'http://localhost:3000/parts', params={'name': name})
     if response.status_code == 200:
@@ -33,7 +33,7 @@ def main():
         elif choice == '3':
             break
         else:
-            print("Invalid input")
+            print("Invalid input!")
 
 if __name__ == '__main__':
     main()
